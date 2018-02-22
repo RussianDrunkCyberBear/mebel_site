2cf1141956d7613ebaa977b880430470bf60f0ff
RussianDrunkCyberBear <russiandrunkcyberbear@gmail.com>
2018-02-22 09:37:30 +0700

    220218

--- home/views.py
+++ home/views.py
@@ -8,5 +8,5 @@ import random
 # Create your views here.
 
 def home(request):
-	products = Product.objects.filter(is_active=True).order_by('?')[:20]
+	products = Product.objects.filter(is_active=True, on_main_page=True)[:20]
 	return render(request, 'home/home.html', locals())
